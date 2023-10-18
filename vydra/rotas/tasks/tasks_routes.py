88a4aa8e88e12feff7e7803136a78be8f54970f9
unknown <you@example.com>
2023-10-18 18:06:02 -0300

    Add task routes

--- vydra/rotas/tasks/tasks_routes.py
+++ vydra/rotas/tasks/tasks_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from datetime import datetime
-from vydra.models import KeyResults, Objectives, Tasks
+from vydra.models import KeyResults, Objectives, Tasks, Postsql
 from app import db
 from vydra.rotas.authentication.authentication import token_required
 
@@ -130,3 +130,81 @@ def deletar_tarefa(id):
     db.session.commit()
 
     return jsonify({'message': 'Tarefa deletada com sucesso!'})
+
+#LISTA A TASK POR KEY_RESULT_ID
+@task_routes_bp.route('/tasks/task_kr', methods=['GET'])
+def listar_tasks_kr_id():
+    
+    banco = Postsql('dpg-cjju8uuphtvs73eff01g-a', 'vydra_96oh', 'vydra_96oh_user', "LNZSNaXgaB2tnD51TY8eHxNgeJ5PK8zg")
+
+    key_result_id = request.args.get('key_result_id')
+
+    query = f'''
+    SELECT json_agg(t) AS tasks
+    FROM (
+        SELECT * 
+        FROM tasks
+        WHERE key_result_id = {key_result_id}
+    ) t;
+    '''
+
+    dados = banco.query(query)
+
+    return jsonify(dados)
+
+#LISTA TODOS OS OBJETIVOS, KEY_RESULTS E TASKS POR TEAM_ID
+@task_routes_bp.route('/tasks/all_tasks', methods=['GET'])
+def listar_obj_kr_task():
+    
+    banco = Postsql('dpg-cjju8uuphtvs73eff01g-a', 'vydra_96oh', 'vydra_96oh_user', "LNZSNaXgaB2tnD51TY8eHxNgeJ5PK8zg")
+
+    team_id = request.args.get('team_id')
+
+    query = f'''
+    SELECT json_build_object(
+    'objective_id', objectives.id,
+    'objective_name', objectives.name,
+    'objective_description', objectives.description,
+    'created_at', objectives.created_at,
+    'prevision_date', objectives.prevision_date,
+    'finished_at', objectives.finished_at,
+    'status', objectives.status,
+    'percentage', objectives.percentage,
+    'team_id', objectives.team_id,
+    'key_results', (
+        SELECT json_agg(json_build_object(
+            'key_result_id', key_results.id,
+            'key_result_name', key_results.name,
+            'key_result_description', key_results.description,
+            'created_at', key_results.created_at,
+            'prevision_date', key_results.prevision_date,
+            'finished_at', key_results.finished_at,
+            'weight', key_results.weight,
+            'percentage', key_results.percentage,
+            'objective_id', key_results.objective_id,
+            'responsable', key_results.responsable,
+            'tasks', (
+                SELECT json_agg(json_build_object(
+                    'task_id', tasks.id,
+                    'task_name', tasks.name,
+                    'task_description', tasks.description,
+                    'task_created_at', tasks.created_at,
+                    'task_prevision_date', tasks.prevision_date,
+                    'task_finished_at', tasks.finished_at,
+                    'task_status', tasks.status
+                ))
+                FROM tasks
+                WHERE tasks.key_result_id = key_results.id
+            )
+        ))
+        FROM key_results
+        WHERE key_results.objective_id = objectives.id
+    )
+    )
+    FROM objectives
+    WHERE objectives.team_id = {team_id};
+    '''
+
+    dados = banco.query(query)
+
+    return jsonify(dados)
